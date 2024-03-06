aef2317567e7c50b0f84fa7bfee415309f3d6a35
MrFairbunkle <77782018+MrFairbunkle@users.noreply.github.com>
2024-03-06 09:32:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import random, time 
+name=str(input("Welcome.\nWhat is your name?"))
+print(f"Ah, {name}, what a lovely name.")
 
+def menu():
+    choice=input("A. Campaign\nB.Heroes\nC.Summoning\n ")
