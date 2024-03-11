67a70f2ef79d505d88ccdb95f3b9e109999ddc89
MrFairbunkle <77782018+MrFairbunkle@users.noreply.github.com>
2024-03-11 15:01:36 +0000

    ifchoice

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ name=str(input("Welcome.\nWhat is your name?"))
 print(f"Ah, {name}, what a lovely name.")
 
 def menu():
-    choice=input("A. Campaign\nB.Heroes\nC.Summoning\n ")
+    choice=input("A. Campaign\nB.Heroes\nC.Summoning\n ").lower()
+    if choice=="a":
+        print("\n\n")
+    elif choice=="b":
+        print("\n\n")
+    elif choice=="c":
+        print("\n\n")
